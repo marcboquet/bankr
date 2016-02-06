2b3c9ad99a77e43689bc2719e165d11045245a54
Marc Boquet <marc.boquet@gmail.com>
2016-02-06 14:20:22 +0100

    Warn if there's missing ENV vars

--- test_against_real_website.rb
+++ test_against_real_website.rb
@@ -6,12 +6,22 @@ require 'pp'
 require 'yaml'
 
 puts 'LaCaixa Scraper Live Test'
-scraper = Bankr::Client.new('la_caixa', login: ENV['LOGIN'], password: ENV['PASSWORD'])
-account_number = ENV['ACCOUNT_NUMBER']
+if !ENV['LOGIN'] || !ENV['PASSWORD']
+  puts "ENV['LOGIN'] or ENV['PASSWORD'] not supplied."
+else
+  scraper = Bankr::Client.new('la_caixa', login: ENV['LOGIN'], password: ENV['PASSWORD'])
+  account_number = ENV['ACCOUNT_NUMBER']
 
-puts 'Fetching movements for the last week...'
-movements = scraper.movements_until(account_number, 1.week.ago)
+  if !account_number
+    accounts = scraper.list_accounts
+    puts "ENV['ACCOUNT_NUMBER'] not supplied. Found these accounts:"
+    puts accounts
+  else
+    puts 'Fetching movements for the last week...'
+    movements = scraper.movements_until(account_number, 1.week.ago)
 
-puts "Fetched #{movements.size} movements."
-puts movements.inspect
-puts movements.reject(&:nil?).map(&:to_hash)
+    puts "Fetched #{movements.size} movements."
+    puts movements.inspect
+    puts movements.reject(&:nil?).map(&:to_hash)
+  end
+end
